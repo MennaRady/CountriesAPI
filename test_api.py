d26c19b67ae6c728e14ddecd8af1885569959dce
Menatallah <menatalla.mohamed@gmail.com>
2020-03-18 03:47:36 +0200

    Test class added

---
+++ test_api.py
@@ -0,0 +1,11 @@
+from api import getBySpec1
+import requests
+import unittest
+
+class MyTestCase(unittest.TestCase):
+
+    def test_getBySpec1(self):
+        self.assertEquals()
+
+if __name__ == '__main__':
+    unittest.main()
