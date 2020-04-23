af1e895a3d72213e2f9ec941ddd26cf66060fe53
Menatallah <menatalla.mohamed@gmail.com>
2020-04-23 22:22:31 +0200

    caching logic added

--- baseApi.py
+++ baseApi.py
@@ -1,7 +1,10 @@
 import requests
 import json
+import requests_cache
 
 class baseApi:
+
+    requests_cache.install_cache('country_caches', backend='memory', expire_after=300)
 
     def __init__(self):
         self.URL = 'https://restcountries.eu/rest/v2/name/'
@@ -19,8 +22,10 @@ class baseApi:
     def getCountryInfo(self,country):
         api_url = self.URL+country
         response = requests.get(api_url)
+        print (response.from_cache)
 
         if response.status_code == 200:
-            return json.loads(response.content)
+            return (json.loads(response.content), response.from_cache)
+
         else:
             return None
